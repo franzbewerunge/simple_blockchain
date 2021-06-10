ee1afa208c83e38fe45392d36f122ca032037c22
Franz Bewerunge <bewerunge.franz@gmail.com>
2021-06-10 17:08:00 +0200

    Init Blockchain

---
+++ blockchain.py
@@ -0,0 +1,23 @@
+import hashlib
+
+class Blockchain:
+
+    def __init__(self):
+        self.chain = []
+
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Creates a new transaction to go into the next mined Block
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hold this transaction
+        """
+
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last
